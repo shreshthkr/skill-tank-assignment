9a6d0886fc1af0eb8e2d96fddced187c0e2f7c25
unknown <Shreshth Kumar>
2023-09-25 13:33:31 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {connection} = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, async () => {
+    try {
+      await connection;
+      console.log("Connected to mongoDB");  
+    } catch (error) {
+        console.log("Cannot connect to DB")
+        console.log(error);
+    }
+    console.log(`Server running on port ${process.env.PORT}`)
+});
