eac555013f6b03ff1d871f9e8d482a750921efc2
unknown <Shreshth Kumar>
2023-09-26 20:27:10 +0530

    added login signup backend

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt=require("jsonwebtoken")
+
+const auth=(req,res,next)=>{
+    const token=req.headers.authorization
+    if(token){
+       const decoded = jwt.verify(token, "rmentor")
+       if(decoded){
+        req.body.userID=decoded.userID
+        next()
+       }else{
+        res.status(400).send({"msg": "Please Login First"})
+       }
+    }else{
+        res.status(400).send({"msg": "Please Login First"})
+       }
+}
+
+
+module.exports={
+    auth
+}
---
+++ backend/models/users.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+//User Schema
+const userSchema = mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  UserModel,
+};
